ce1b36130b74b91862b8c2149bb01a187cf13918
polinage <103887459+polinage@users.noreply.github.com>
2022-06-28 23:18:12 +0300

    Add files via upload

---
+++ jScripts/JavaScript.js
@@ -0,0 +1,37 @@
+﻿var slideIndex = 0;
+showSlides(slideIndex);
+
+// פונקציה לחצים
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// פונקציה לתמונות הקטנות
+function currentPhoto(n) {
+    showSlides(slideIndex = n);
+}
+
+// הפונקציה הראשית
+function showSlides(n) {
+    var photos = document.getElementsByClassName("bigPhoto"); //משתנה לתמונה הגדולה
+    var thumbnails = document.getElementsByClassName("smallPhoto"); //משתנה לתמונות הקטנות 
+    var captionText = document.getElementById("caption"); //משתנה לכותרת של התמונה
+
+    if (n >= photos.length) {  
+        slideIndex = 0; //אם מספר התמונה הגיע ל-6, תחזור לתמונה הראשונה
+    }
+    if (n < 0) {  
+        slideIndex = photos.length - 1; // אם המשתמש לחץ הקודם בתמונה הראשונה, תחזיר אותו לסוף
+    }
+    for (i = 0; i < photos.length; i++) { 
+        photos[i].style.display = "none"; // תעבור על כל התמונות ותסתיר אותן
+    }
+    for (i = 0; i < thumbnails.length; i++) {
+        thumbnails[i].className = thumbnails[i].className.replace(" active", ""); // תחליף את class " active" בריק
+    }
+    photos[slideIndex].style.display = "block"; // תציג את התמונה הנוכחית
+    thumbnails[slideIndex].className += " active"; //  class " active" תוסיך לתמונה הקטנה
+    captionText.innerHTML = thumbnails[slideIndex].alt; // הערך של הכותרת שווה לערך של ה-alt של התמונה
+}
+
+
---
+++ jScripts/JavaScript2.js
@@ -0,0 +1,28 @@
+﻿////$(document).ready(function () {
+////    $(window).scroll(function () {
+////        if ($(this).scrollTop() > 20) {
+////            $('#scrollup').fadeIn();
+////        }
+////        else {
+////            $('#scrollup').fadeout();
+////        }
+////    });
+////    $("#scrollup").click(function () {
+////        $('html, body').animate({ scrollTop: 0 }, 800);
+////    });
+////});
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        $('#scrollup').fadeIn();
+    } else {
+        $('#scrollup').fadeOut();
+    }
+}
+
+function topFunction() {
+    $('html, body').animate({ scrollTop: 0 }, 500);
+    return false;
+}
